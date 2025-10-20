446eb0ef39f0fe68818fdc66406559c0b9be5cb5
Laslo76 <162439161+Laslo76@users.noreply.github.com>
2025-10-20 15:01:04 +0300

    Update CustomServices.java
    Изменено статическое поле класса CustomServices согласно требований

--- ru/netology/services/CustomServices.java
+++ ru/netology/services/CustomServices.java
@@ -1,10 +1,11 @@
 package ru.netology.services;
 
 public class CustomServices {
-    final static int bet = 100;
+    final static int TAX_RATE = 100;
 
     public static int feeCalc(int price, int weight) {
 
-        return (int) (price / 100 + weight * bet);
+        return (int) (price / 100 + weight * TAX_RATE);
     }
 }
+
